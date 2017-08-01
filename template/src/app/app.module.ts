54d1c7485de8536ef07364910412be80206e69c5
jpbretanha <jpbretanha@gmail.com>
2017-08-01 11:56:16 -0300

    fixed module error for AoT build

--- template/src/app/app.module.ts
+++ template/src/app/app.module.ts
@@ -1,7 +1,3 @@
-import { NgModuleFactory, NgModuleFactoryLoader, RendererFactory2, NgZone } from '@angular/core';
-import { ServerModule, ɵServerRendererFactory2 } from '@angular/platform-server';
-import { ɵAnimationEngine } from '@angular/animations/browser';
-import { NoopAnimationsModule, ɵAnimationRendererFactory } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -9,35 +5,6 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 export { AppComponent };
-
-// declarations
-export function instantiateServerRendererFactory(
-  renderer: RendererFactory2, engine: ɵAnimationEngine, zone: NgZone) {
-   return new ɵAnimationRendererFactory(renderer, engine, zone);
-}
-const createRenderer = ɵServerRendererFactory2.prototype.createRenderer;
-ɵServerRendererFactory2.prototype.createRenderer = function () {
-  const result = createRenderer.apply(this, arguments);
-  const setProperty = result.setProperty;
-  result.setProperty = function () {
-    try {
-      setProperty.apply(this, arguments);
-    } catch (e) {
-      if (e.message.indexOf('Found the synthetic') === -1) {
-        throw e;
-      }
-    }
-  };
-  return result;
-}
-
-export const SERVER_RENDER_PROVIDERS = [
-  {
-    provide: RendererFactory2,
-    useFactory: instantiateServerRendererFactory,
-    deps: [ɵServerRendererFactory2, ɵAnimationEngine, NgZone]
-  }
-];
 
 @NgModule({
   declarations: [
@@ -53,7 +20,7 @@ export const SERVER_RENDER_PROVIDERS = [
     HttpModule
   ],
   exports: [AppComponent],
-  providers: [SERVER_RENDER_PROVIDERS],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- template/src/app/app.server.module.ts
+++ template/src/app/app.server.module.ts
@@ -1,8 +1,40 @@
 /// <reference types="node" />
+import { NgModule, NgModuleFactory, NgModuleFactoryLoader, RendererFactory2, NgZone } from '@angular/core';
+import { ServerModule, ɵServerRendererFactory2 } from '@angular/platform-server';
+import { ɵAnimationEngine } from '@angular/animations/browser';
+import { NoopAnimationsModule, ɵAnimationRendererFactory } from '@angular/platform-browser/animations';
 
-import { NgModule, NgModuleFactory, NgModuleFactoryLoader } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
 import { AppModule, AppComponent } from './app.module';
+
+// declarations
+export function instantiateServerRendererFactory(
+  renderer: RendererFactory2, engine: ɵAnimationEngine, zone: NgZone) {
+   return new ɵAnimationRendererFactory(renderer, engine, zone);
+}
+  
+const createRenderer = ɵServerRendererFactory2.prototype.createRenderer;
+ɵServerRendererFactory2.prototype.createRenderer = function () {
+  const result = createRenderer.apply(this, arguments);
+  const setProperty = result.setProperty;
+  result.setProperty = function () {
+    try {
+      setProperty.apply(this, arguments);
+    } catch (e) {
+      if (e.message.indexOf('Found the synthetic') === -1) {
+        throw e;
+      }
+    }
+  };
+  return result;
+}
+
+export const SERVER_RENDER_PROVIDERS = [
+  {
+    provide: RendererFactory2,
+    useFactory: instantiateServerRendererFactory,
+    deps: [ɵServerRendererFactory2, ɵAnimationEngine, NgZone]
+  }
+];
 
 export class ServerFactoryLoader extends NgModuleFactoryLoader {
   load(path: string): Promise<NgModuleFactory<any>> {
@@ -16,12 +48,14 @@ export class ServerFactoryLoader extends NgModuleFactoryLoader {
 
 @NgModule({
   imports: [
+    NoopAnimationsModule,
     ServerModule,
     AppModule
   ],
   bootstrap: [AppComponent],
   providers: [
-    { provide: NgModuleFactoryLoader, useClass: ServerFactoryLoader }
+    { provide: NgModuleFactoryLoader, useClass: ServerFactoryLoader },
+    SERVER_RENDER_PROVIDERS //comentar essa linha se as rotas tiverem recarregando a pagina
   ]
 })
 export class AppServerModule { }
